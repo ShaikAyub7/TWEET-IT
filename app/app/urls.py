2a089daf1807860f65dc817327d9027dd545cbce
ShaikAyub7 <skayub929@gmail.com>
2024-07-21 13:45:20 +0530

    save

--- app/app/urls.py
+++ app/app/urls.py
@@ -26,10 +26,6 @@ urlpatterns = [
     path('accounts/',include('django.contrib.auth.urls')),
     # path('api-auth/', include('rest_framework.urls')),
 
-
-
-
-
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
